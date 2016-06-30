a43568f0c5111286dc723fabc6a434764aa252f3
hellno-7 <dayalsuv@yahoo.com>
2016-06-30 18:10:07 +0530

    Update LaunchBrowser.java

--- src/test/java/tests/LaunchBrowser.java
+++ src/test/java/tests/LaunchBrowser.java
@@ -1,8 +1,11 @@
 package tests;
 
+import java.net.URL;
 import java.util.concurrent.TimeUnit;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.Test;
 
 
@@ -17,13 +20,14 @@ public class LaunchBrowser {
 			capability = DesiredCapabilities.chrome();
 			System.setProperty("webdriver.chrome.driver", "C:\\Users\\c5245770\\Downloads\\chromedriver.exe");
 			//driver = new ChromeDriver();
-			driver = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), capability);
-			driver.get("https://google.com");
-			driver.manage().window().maximize();
-			driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS); 			 		
-			driver.quit();
+			driver = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), capability);	
+			//driver = new ChromeDriver();
+		  	driver.get("https://google.com");
+		 	driver.manage().window().maximize();
+		 	driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS); 			 		
+		 	driver.quit();
 
-
+		 	
 		} catch (Exception e) {
 			 e.printStackTrace();
 		}
